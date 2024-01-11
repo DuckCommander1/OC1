b07f01a5d96bcd10abf0176edb1f1546a80b5fb5
Alex Ga <alexandrga3@gmail.com>
2024-01-11 19:26:37 +0300

    Добавьте файлы проекта.

---
+++ Multithread1/Multithread1.cpp
@@ -0,0 +1,66 @@
+﻿#include <windows.h>
+#include <iostream>
+using namespace std;
+
+struct Data
+{
+	int res, sz;
+	int* M;
+};
+
+void thread(void* A) //Finds smalest int in array
+{
+	Data* n = (Data*)A;
+	n->res = n->M[0];
+	for (int i = 0; i < n->sz; i++)
+	{
+		Sleep(100);
+		if (n->M[i] < n->res)
+		{
+			n->res = n->M[i];
+		}
+	}
+
+	A = (void*)n;
+}
+
+int main()
+{
+	HANDLE	Hthread;
+	DWORD	IDthread;
+	srand(time(0));
+	setlocale(LC_ALL, "rus");
+
+	int sz, res = INT32_MIN;
+	cout << "Размер массива:" << endl;
+	cin >> sz;
+
+	if (sz < 1) {
+		cout << "Invalid size";
+		return -1;
+	}
+
+	int* a = new int[sz];
+
+	for (int i = 0; i < sz; i++)
+	{
+		cin >> a[i];
+		//cout << a[i] << " ";
+	}
+
+	Data* data = new Data{ res, sz, a };
+
+	Hthread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)thread, (void*)data, 0, &IDthread);
+	if (Hthread == NULL) {
+		cout << "Error in thread creation!";
+		return GetLastError();
+	}
+	WaitForSingleObject(Hthread, INFINITE);
+	
+	CloseHandle(Hthread);
+	cout << "Наименьшее число:" << data->res;
+	
+	delete[] a;
+	delete data;
+	return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,65 @@
+﻿#include <string>
+#include <iostream>
+#include "pch.h"
+#include "CppUnitTest.h"
+
+#include "../Multithread1/Multithread1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		//Я попытался, но почему-то я получаю разные ответы в тесте и в .exe! Магия :)
+		// 
+		//TEST_METHOD(TestMethod1)
+		//{
+		//	int expectedMin = 5;
+		//	int testArray[] = { 5, 10, 54, 9999, 7 };
+
+		//	std::stringstream input("5 5 10 54 9999 7");
+		//	std::stringstream output;
+
+		//	cin.rdbuf(input.rdbuf());
+		//	cout.rdbuf(output.rdbuf());
+		//	int a = main();
+		//	
+		//	int res = stoi(output.str().substr(33));
+
+		//	Assert::AreEqual(expectedMin, res);
+		//}
+
+		TEST_METHOD(TestMethod2) {
+			int expectedMin = -98;
+			string testArray = "7 1234 -79 54 9999 -81 341 -98";
+
+			std::stringstream input(testArray);
+			std::stringstream output;
+
+			cin.rdbuf(input.rdbuf());
+			cout.rdbuf(output.rdbuf());
+			int a = main();
+
+			int res = stoi(output.str().substr(33));
+
+			Assert::AreEqual(expectedMin, res);
+		}
+
+		TEST_METHOD(TestMethodError) {
+			string expected = "Invalid size";
+			string testArray = "-7 1 -17 45";
+
+			std::stringstream input(testArray);
+			std::stringstream output;
+
+			cin.rdbuf(input.rdbuf());
+			cout.rdbuf(output.rdbuf());
+			int a = main();
+
+			Assert::AreEqual(expected, output.str().substr(16));
+		}
+	};
+}
